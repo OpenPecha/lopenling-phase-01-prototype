8905f6f6c53cec9154abf9308e0e4e779a342143
kunsang <tenkus47@gmail.com>
2022-12-09 13:29:08 +0530

    only display votes in discourse disabled

--- app/routes/embed/$annotation.tsx
+++ app/routes/embed/$annotation.tsx
@@ -99,6 +99,7 @@ export default function embed() {
           name="_action"
           value="likeVote"
           type="submit"
+          disabled={!loaderData.user}
           className="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           {loaderData.likeCount}
@@ -106,6 +107,7 @@ export default function embed() {
         <button
           name="_action"
           value="dislikeVote"
+          disabled={!loaderData.user}
           type="submit"
           className="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
