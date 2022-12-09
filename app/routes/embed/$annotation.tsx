e7be290c26fda5ff51c4a4d89e2711e8d28f170b
kunsang <tenkus47@gmail.com>
2022-12-09 13:32:47 +0530

    only display votes in discourse disabled with emoji

--- app/routes/embed/$annotation.tsx
+++ app/routes/embed/$annotation.tsx
@@ -102,7 +102,7 @@ export default function embed() {
           disabled={!loaderData.user}
           className="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          {loaderData.likeCount}
+          {loaderData.likeCount}👍
         </button>
         <button
           name="_action"
@@ -111,7 +111,7 @@ export default function embed() {
           type="submit"
           className="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          {loaderData.dislikeCount}
+          {loaderData.dislikeCount}👎
         </button>
       </Form>
     </>
