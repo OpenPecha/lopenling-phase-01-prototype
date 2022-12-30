82adf62d0b3aa317ee1fe8fbcb76e9e760a9cae0
kunsang <tenkus47@gmail.com>
2022-12-30 14:19:37 +0530

    audio list show test 2

--- app/routes/api/audio.tsx
+++ app/routes/api/audio.tsx
@@ -27,22 +27,22 @@ export const action: ActionFunction = async ({ request }) => {
     unstable_createFileUploadHandler({
       maxPartSize: 5_000_000,
       file: ({ filename }) => {
-        // setTimeout(() => {
-        //   fs.unlink(`file/${filename}.webm`, function (err) {
-        //     if (err && err.code == "ENOENT") {
-        //       // file doens't exist
-        //       console.info("File doesn't exist, won't remove it.");
-        //     } else if (err) {
-        //       // other errors, e.g. maybe we don't have enough permission
-        //       console.error("Error occurred while trying to remove file");
-        //     } else {
-        //       console.info(`removed`);
-        //     }
-        //   });
-        // }, 4000);
+        setTimeout(() => {
+          fs.unlink(`./app/file/${filename}.webm`, function (err) {
+            if (err && err.code == "ENOENT") {
+              // file doens't exist
+              console.info("File doesn't exist, won't remove it.");
+            } else if (err) {
+              // other errors, e.g. maybe we don't have enough permission
+              console.error("Error occurred while trying to remove file");
+            } else {
+              console.info(`removed`);
+            }
+          });
+        }, 4000);
         return `${filename}.webm`;
       },
-      directory: `file`,
+      directory: `./app/file`,
     }),
     // parse everything else into memory
     unstable_createMemoryUploadHandler()
